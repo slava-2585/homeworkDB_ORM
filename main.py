03a47757fbfe95064550b3a260fbb671728c0b75
Nikiforov Vyacheslav <slava-2585@yandex.ru>
2023-08-29 13:30:45 +0300

    Upload_data

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
-from models import create_table
+from models import create_table, Publisher, Book, Shop, Stock, Sale, upload_data
 
 
 DSN = 'postgresql://postgres:12345@localhost:5432/library_book_db'
 engine = sqlalchemy.create_engine(DSN)
+Session = sessionmaker(bind=engine)
+
+def change_publisher(Session):
+    author = input('Введите автора для поиска ')
+
+    session = Session()
+    q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name.like(author))
+    #print ([s[0]] + [s[1]] + [str(s[2])] + [str(s[3])] for s in q.all())
+    for s in q.all():
+        print(f'{s[0]} | {s[1]} | {str(s[2])} | {str(s[3])}')
+    session.close()
 
 if __name__ == '__main__':
     create_table(engine)
-
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# session.close()
-
+    upload_data(engine)
+    change_publisher(Session)
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+import json
 
 Base = declarative_base()
 
@@ -9,14 +10,22 @@ class Publisher(Base):
     id_publisher = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
     
+    def __str__(self):
+        return f'{self.name}'
+    
+    
 class Book(Base):
     __tablename__ = 'book'
     
     id_book = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(length=30))
+    title = sq.Column(sq.String(length=50))
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id_publisher'))
     
     publisher = relationship(Publisher, backref = 'book')
+    
+    def __str__(self):
+        return f'{self.title}'
+    
     
 class Shop(Base):
     __tablename__ = 'shop'
@@ -24,6 +33,10 @@ class Shop(Base):
     id_shop = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
     
+    def __str__(self):
+        return f'{self.name}'
+    
+        
 class Stock(Base):
     __tablename__ = 'stock'
     
@@ -34,17 +47,52 @@ class Stock(Base):
     
     book = relationship(Book, backref = 'stock')
     shop = relationship(Shop, backref = 'stock')
+
     
 class Sale(Base):
     __tablename__ = 'sale'
     
     id_sale = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Float)
+    price = sq.Column(sq.String(length=10))
     date_sale = sq.Column(sq.DateTime)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id_stock'))
+    count = sq.Column(sq.Integer)
     
     stock = relationship(Stock, backref = 'sale')
+    
+    def __str__(self):
+        return f'{self.price}, {self.date_sale}'
+    
     
 def create_table(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+    
+def upload_data(engine):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    with open('tests_data.json') as json_file:
+        data = json.load(json_file)
+        for s in data:
+            if s['model'] == 'publisher':
+                d = Publisher(id_publisher = s['pk'], name = s['fields']['name'])
+                session.add(d)
+                session.commit()
+            elif s['model'] == 'book':
+                d = Book(id_book = s['pk'], title = s['fields']['title'], id_publisher = s['fields']['id_publisher'])
+                session.add(d)
+                session.commit()
+            elif s['model'] == 'shop':
+                d = Shop(id_shop = s['pk'], name = s['fields']['name'])
+                session.add(d)
+                session.commit()
+            elif s['model'] == 'stock':
+                d = Stock(id_stock = s['pk'], id_book = s['fields']['id_book'], id_shop = s['fields']['id_shop'], count = int(s['fields']['count']))
+                session.add(d)
+                session.commit()
+            elif s['model'] == 'sale':
+                d = Sale(id_sale = s['pk'], price = s['fields']['price'], date_sale = s['fields']['date_sale'], count = int(s['fields']['count']), id_stock = int(s['fields']['id_stock']))
+                session.add(d)
+                session.commit()
+    session.close()
