d8463dbbb2476b143e34061f716fb1dfe089926c
Nikiforov Vyacheslav <slava-2585@yandex.ru>
2023-09-01 13:37:08 +0300

    fix def change_publisher

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 from models import create_table, Publisher, Book, Shop, Stock, Sale, upload_data
 
@@ -8,17 +9,19 @@ DSN = 'postgresql://postgres:12345@localhost:5432/library_book_db'
 engine = sqlalchemy.create_engine(DSN)
 Session = sessionmaker(bind=engine)
 
-def change_publisher(Session):
-    author = input('Введите автора для поиска ')
-
+def change_publisher(Session, author):
     session = Session()
-    q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name.like(author))
-    #print ([s[0]] + [s[1]] + [str(s[2])] + [str(s[3])] for s in q.all())
-    for s in q.all():
-        print(f'{s[0]} | {s[1]} | {str(s[2])} | {str(s[3])}')
+    sub = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale)
+    if author.isdigit():
+        q = sub.filter(Publisher.id_publisher == int(author))
+    else:
+        q = sub.filter(Publisher.name.like(author))
+    for title_book, name_shop, price_sale, date_sale in q.all():
+        print(f'Название книги: {title_book} Название магазина: {name_shop} Стоимость: {str(price_sale)} Дата продажи: {date_sale.strftime("%d-%m-%Y")}')
     session.close()
 
 if __name__ == '__main__':
     create_table(engine)
     upload_data(engine)
-    change_publisher(Session)
+    author = input('Введите имя автора или его ID для поиска ')
+    change_publisher(Session, author)
